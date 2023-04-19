6034bc77a98eea6bbc32aa8a36762371244c82ea
kimyoungyin <yin199859@gmail.com>
2023-04-19 21:16:05 +0900

    etc: model.ts 필요없는 주석 제거

--- src/models.ts
+++ src/models.ts
@@ -8,19 +8,6 @@ import Sequelize, {
 } from "sequelize";
 import sequelize from "database";
 
-// sequelize model = musql table
-// export const Category = sequelize.define(
-//     "category",
-//     {
-//         // id는 sequelize에서 자동으로 생김
-//         name: {
-//             type: Sequelize.STRING(10),
-//             allowNull: false,
-//         },
-//     },
-//     { timestamps: false }
-// ); // createdAt, updatedAT 필요 없음
-
 export class Category extends Model<
     InferAttributes<Category>,
     InferCreationAttributes<Category>
@@ -130,46 +117,3 @@ Choice.init(
     },
     { tableName: "choices", timestamps: false, sequelize }
 );
-
-// export const Post = sequelize.define("post", {
-//     // id는 sequelize에서 자동으로 생김
-//     title: {
-//         type: Sequelize.STRING(20), // 20자 제한 추가함
-//         allowNull: false,
-//     },
-//     choice1: {
-//         type: Sequelize.STRING(20), // 20자 제한 추가
-//         allowNull: false,
-//     },
-//     choice1Url: {
-//         type: Sequelize.STRING,
-//         allowNull: true,
-//     },
-//     choice2: {
-//         type: Sequelize.STRING(20), // 20자 제한 추가
-//         allowNull: false,
-//     },
-//     choice2Url: {
-//         type: Sequelize.STRING,
-//         allowNull: true,
-//     },
-//     uploaderId: {
-//         type: Sequelize.STRING(28),
-//         allowNull: false,
-//     },
-// });
-
-// export const Choice = sequelize.define(
-//     "choice",
-//     {
-//         uid: {
-//             type: Sequelize.STRING(28), // uid 길이
-//             allowNull: false,
-//         },
-//         choiceType: {
-//             type: Sequelize.BOOLEAN, // 1 혹은 0
-//             allowNull: false,
-//         },
-//     },
-//     { timestamps: false } // createdAt, updatedAT 필요 없음
-// );
