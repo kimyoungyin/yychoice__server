63569d43ea40272f520f469d926b03c7bad523a8
kimyoungyin <yin199859@gmail.com>
2023-07-19 22:40:24 +0900

    fix: cors whitelist 수정

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // body 접근 가능
 
-const whitelist = ["http://localhost:3000", "https://yychoice.netlify.app"];
+const whitelist = [
+    "http://localhost:3000",
+    "https://develop--yychoice.netlify.app",
+];
 const corsOptions: CorsOptions = {
     credentials: true,
     optionsSuccessStatus: 200,
